4427083af7a45e4a44a4ae9dc022151c0c03a3f9
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-02-19 20:34:41 -0700

    Vista de Registro

---
+++ app/src/main/java/gamez/ana/thecheezery/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package gamez.ana.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
