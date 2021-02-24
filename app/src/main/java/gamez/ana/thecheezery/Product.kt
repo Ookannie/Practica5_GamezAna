e767c3527fea2636fad62d701b66e5f7092e96a5
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-02-24 16:19:39 -0700

    Plantilla Productos

---
+++ app/src/main/java/gamez/ana/thecheezery/Product.kt
@@ -0,0 +1,5 @@
+package gamez.ana.thecheezery
+
+data class Product (var name: String, var image: Int, var description: String, var  price: Double) {
+
+}
--- app/src/main/java/gamez/ana/thecheezery/ProductosActivity.kt
+++ app/src/main/java/gamez/ana/thecheezery/ProductosActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ProductosActivity : AppCompatActivity() {
+    var coldDrinks = ArrayList<Product>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_productos)
+    }
+
+    fun agregarProductos(){
+        
     }
 }
